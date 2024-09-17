362dddfda3fadeba30ebb7ed7b1c4209c357cfdb
Sumner44 <scotton@css.edu>
2024-09-17 10:50:46 -0500

    Add files via upload

---
+++ bicycle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CisPkg2087.Bicycle
+{
+
+    public class Bicycle
+    {
+        // The Bicycle class has three fields
+        protected int cadence;
+        protected int gear;
+        protected int speed;
+
+        // The Bicycle class has one constructor
+        public Bicycle(int startCadence, int startSpeed, int startGear)
+        {
+            gear = startGear;
+            cadence = startCadence;
+            speed = startSpeed;
+        }
+
+        // The Bicycle class has four methods
+        public void SetCadence(int newValue)
+        {
+            cadence = newValue;
+        }
+
+        public void SetGear(int newValue)
+        {
+            gear = newValue;
+        }
+
+        public int GetCadence()
+        {
+            return this.cadence;
+        }
+
+        public int GetGear()
+        {
+            return this.gear;
+        }
+
+        public int GetSpeed()
+        {
+            return this.speed;
+        }
+
+        public void ApplyBrake(int decrement)
+        {
+            speed -= decrement;
+        }
+
+        public void SpeedUp(int increment)
+        {
+            speed += increment;
+        }
+
+        public override string ToString()
+        {
+            string bike = $"My Bike: {this.speed}, {GetCadence()}, {this.gear}";
+            return bike;
+        }
+    }
+}
